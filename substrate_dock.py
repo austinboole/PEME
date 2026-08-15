9ec0280cbfb4cb6a63e34a270f222f1122848725
Austin Boole <austin.boole@gmail.com>
2026-08-15 16:18:53 +0100

    Determines what the 'target' atom for a specific reaction in any substrate is

--- substrate_dock.py
+++ substrate_dock.py
@@ -36,18 +36,52 @@ def parse_pdb(file_path):
                         graph[source_id].append(neighbour_id)   #Adds the neighbour atom to the source atom if not already present
                     if source_id not in graph[neighbour_id]:
                         graph[neighbour_id].append(source_id)   #Adds the original source atom to the neighbour atom if not already present
+
+        return atoms,graph
                     
 
-    print(graph)
-                
-                
+REACTION_RULES = {
+    "phosphate_hydrolysis":{
+        "target_element": "P",
+        "required_neighbours": ["O","O","O","O"]
+    }
 
 
-                
+}         
 
-               
+def find_target_atom(atoms,graph,rule):
+    target_elem = rule["target_element"]
+    req_neighbours = sorted(rule["required_neighbours"])
+
+    matched_targets = []
+
+    for atom_id, data in atoms.items():
+        if data["element"] != target_elem:
+            continue
+
+        neighbours = graph.get(atom_id,[])
+        if len(neighbours) != len(req_neighbours):
+            continue
+
+        neighbour_elements = []
+        for nbr_id in neighbours:
+            if nbr_id in atoms:
+                neighbour_elements.append(atoms[nbr_id]["element"])
+
+        if sorted(neighbour_elements) == req_neighbours:
+            matched_targets.append(atom_id)
+
+    return matched_targets
 
 
-parse_pdb(r"C:\Users\Austin Boole\OneDrive\Documents\PEME\inputs\a73c0b85-d39c-4b0d-be5e-26a31ef19321.pdb")
 
+# --------------------
+#      EXECUTION
+# --------------------
+file_path = r"C:\Users\Austin Boole\OneDrive\Documents\PEME\inputs\a73c0b85-d39c-4b0d-be5e-26a31ef19321.pdb" #Substrate is G6P in this instance
 
+atoms, graph = parse_pdb(file_path)
+targets = find_target_atom(atoms, graph, REACTION_RULES["phosphate_hydrolysis"])
+print("Target Atom IDs:",targets)
+        
+    
